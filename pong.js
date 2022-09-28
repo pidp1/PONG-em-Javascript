f39ad754560d605ba116cd400535db386574419d
pidp1 <pedro.imparato@gmail.com>
2022-09-28 15:32:58 -0300

    update de organização

--- pong.js
+++ pong.js
@@ -30,9 +30,9 @@ let trilha;
 
 
 function preload(){
-  trilha = loadSound('trilha.mp3');
-  ponto = loadSound("ponto.mp3");
-  raquetada = loadSound ("raquetada.mp3")
+  trilha = loadSound('/sons/trilha.mp3');
+  ponto = loadSound("/sons/ponto.mp3");
+  raquetada = loadSound ("/sons/raquetada.mp3")
 }
 
 function setup() {
